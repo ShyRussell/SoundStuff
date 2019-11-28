8a93b5fe080f005cd2a70d96c3f9bfa975b632ce
JonahSpicher <jonah.spicher@gmail.com>
2019-11-28 02:14:19 -0500

    Made input file, not much else

---
+++ Verilog/input_controls.v
@@ -0,0 +1,12 @@
+module controls(
+  output reg [7:0] vol1, //Information about relative volume
+  output reg [7:0] vol2,
+  output reg [7:0] vol3,
+  output reg [7:0] vol4,
+  output reg [7:0] freq1, //Information about frequency, may not literally be frequency
+  output reg [7:0] freq2,
+  output reg [7:0] freq3,
+  output reg [7:0] freq4
+);
+  //And then actually make the module 
+endmodule
