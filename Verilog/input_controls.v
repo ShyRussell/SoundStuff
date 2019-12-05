e0cf66ade65caf39b22f471be392c14e0f007028
JonahSpicher <jonah.spicher@gmail.com>
2019-12-05 01:13:20 -0500

    Made starting structure for sound card

--- Verilog/input_controls.v
+++ Verilog/input_controls.v
@@ -4,8 +4,8 @@ module controls(
   // output reg [7:0] vol2,
   // output reg [7:0] vol3,
   // output reg [7:0] vol4,
-  output reg [7:0] freq1 //Actually equal to 1/2 a period in clock cycles
-  // output reg [7:0] freq2,
+  output reg [7:0] freq1, //Actually equal to 1/2 a period in clock cycles
+  output reg [7:0] freq2
   // output reg [7:0] freq3,
   // output reg [7:0] freq4
 );
@@ -13,18 +13,24 @@ module controls(
   if (switch === 4'd0) begin
     //Do a pre-loaded thing
       freq1 = 8'd20;
+      freq2 = 8'd20;
       #200
       freq1 = 8'd5;
+      freq2 = 8'd20;
       #60
       freq1 = 8'd1;
+      freq2 = 8'd20;
       #40
       freq1 = 8'd24;
+      freq2 = 8'd20;
       #20
       freq1 = 8'd20;
+      freq2 = 8'd20;
   end
   else begin
     // Otherwise set frequency to the switch's value
     freq1 <= switch;
+    freq2 <= switch;
   end
   end
 endmodule
