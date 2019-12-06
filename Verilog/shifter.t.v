563bd3b0a873250ce6b4229486dfd96e071df69b
Shyheim Russell <shyheim.russell@students.olin.edu>
2019-12-05 21:17:24 -0500

    so yea, easy shifter, yea

--- Verilog/shifter.t.v
+++ Verilog/shifter.t.v
@@ -1,26 +1,21 @@
 `include "shifter.v"
 
 module shift_test ();
-  wire[7:0] square_shifted;
-  reg clk;
-  wire[7:0] square_out;
-  reg[7:0] frequency_control;
-  wire[7:0] mover;
+  wire[7:0] out;
+  reg[7:0] Chl1 = 8'd0;
+  reg[7:0] Chl2 = 8'd0;
+  reg[7:0] Chl3 = 8'd0;
+  reg[7:0] Chl4 = 8'b11111111;
 
-  // Clock generation
-  initial clk=0;
-  always #10 clk = !clk;
-
-  initial mover = 2'd0
-
-  square_wave square_wave(.square_out(square_out),.clk(clk),.frequency_control(frequency_control));
-  shifter shift_square(.square_out(mover), .square_shift(square_shifted))
-
+  shifter shifter(.sound_out(out), .Chl1(Chl1), .Chl2(Chl2), .Chl3(Chl3), .Chl4(Chl4));
   initial begin
     // Dump waveforms to file
     $dumpfile("shift_test.vcd");
     $dumpvars();
-
+    # 100
+    Chl3 = 8'b10101010;
+    
     // End execution after some time delay
     #2000 $finish();
   end
+endmodule
--- Verilog/shifter.v
+++ Verilog/shifter.v
@@ -1,12 +1,13 @@
-`include "sawtooth_wave.v"
-`include "sawtooth_LUT.v"
-`include "square_wave.v"
 module shifter
 (
-  output[7:0] square_shift;
-  input[7:0] square_out;
+  output[7:0] sound_out,
+  // input clk,
+  input[7:0] Chl1,
+  input[7:0] Chl2,
+  input[7:0] Chl3,
+  input[7:0] Chl4
   );
 
-  always @(posedge clk) begin
-    square_out = square_out<<2;
+assign sound_out = (Chl1>>2) + (Chl2>>2) + (Chl3>>2) + (Chl4>>2);
+
 endmodule
