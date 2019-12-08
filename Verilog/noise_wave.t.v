9a2ed44e8b377a91bcb80f68fb6395cf7c8cb48e
JonahSpicher <jonah.spicher@gmail.com>
2019-12-08 12:49:01 -0500

    Tried new random noise generation method

--- Verilog/noise_wave.t.v
+++ Verilog/noise_wave.t.v
@@ -5,19 +5,25 @@ module noise_wave_test ();
 
     wire [7:0] noise_out;
     reg clk;
+    integer f;
 
 
     // Clock generation
     initial clk=0;
     always #10 clk = !clk;
 
+    initial begin
+      f = $fopen("output.txt","w");
+    end
+
+    always @(posedge clk) begin
+      $fwrite(f,"%d\n",  $signed(noise_out));
+    end
 
     // Instantiate fake CPU
     noise_wave #(4) noise_wave (.noise_out(noise_out),.clk(clk));
     // Test sequence
-    initial begin
-
-      // $readmemh("asmtest/branch.text", cpu.memory.mem,0);
+    always begin
 
     	// Dump waveforms to file
     	$dumpfile("noise_wave.vcd");
@@ -25,7 +31,9 @@ module noise_wave_test ();
 
 
     	// End execution after some time delay
-    	#2000 $finish();
+    	#2000
+      $fclose(f);
+      #1 $finish();
     end
 
 endmodule
