b0a171abe68e0cdb5397b65644e2c117b82ac1d5
Sabrina Pereira <sabrina@pereira.com>
2019-12-10 01:21:27 -0500

    Reset is low when frequency_control is 0

--- Verilog/square_wave.t.v
+++ Verilog/square_wave.t.v
@@ -7,17 +7,20 @@ Square Wave Test Bench
 
 module square_wave_test ();
 
-    wire [7:0] square_out;
+    wire square_out;
     reg clk;
     reg  [7:0] frequency_control;
 
     // Clock generation
     initial clk=0;
     always #10 clk = !clk;
-    initial frequency_control=8'd2;
+    initial frequency_control=8'd0;
 
     // Instantiate Square Wave Generator
-    square_wave #(.resolution_bits(8), .counter_width(8),.initial_q(8'b10000000)) square_wave(.square_out(square_out),.clk(clk),.frequency_control(frequency_control));
+    // square_wave #(.resolution_bits(8), .counter_width(8),.initial_q(8'b10000000)) square_wave(.square_out(square_out),.clk(clk),.frequency_control(frequency_control));
+    square_wave #(.resolution_bits(1), .counter_width(8)) square_wave(.square_out(square_out),.clk(clk),.frequency_control(frequency_control));
+
+
 
     // Test sequence
     initial begin
