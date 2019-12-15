0a4d1a1b83e935257fbe4ea23712b8fa3dac24c8
JonahSpicher <jonah.spicher@gmail.com>
2019-12-15 11:57:10 -0500

    Working build_note_string

--- Music/notes_to_hex.py
+++ Music/notes_to_hex.py
@@ -32,13 +32,11 @@ def build_note_string(freqs, channel, length, note, bpm=120):
         l = (120/bpm) * 8
     elif length[0] == 's':
         l = (120/bpm) * 4
-    print(l)
     if len(length) == 2: #Just never make it three please
         if length[1] == 't':
             l *= float(2)/3
         elif length[1] == 'd':
             l *= 1.5
-    print(l)
     l = hex(int(l)).upper()
     if len(l) > 5:
         print("Toooooo biiiiiig")
@@ -55,9 +53,48 @@ def build_note_string(freqs, channel, length, note, bpm=120):
         instr = instr + '0'*(3-len(period[2:])) + period[2:]
     return instr
 
+def wait(channel):
+    return '00' + str(channel-1) + '01000'
+
+def rest(channel, length, bpm=120):
+    instr = '00' + str(channel-1)
+
+    if length[0] == 'w':
+        l = (120/bpm) * 64
+    elif length[0] == 'h':
+        l = (120/bpm) * 32
+    elif length[0] == 'q':
+        l = (120/bpm) * 16
+    elif length[0] == 'e':
+        l = (120/bpm) * 8
+    elif length[0] == 's':
+        l = (120/bpm) * 4
+    if len(length) == 2: #Just never make it three please
+        if length[1] == 't':
+            l *= float(2)/3
+        elif length[1] == 'd':
+            l *= 1.5
+    l = hex(int(l)).upper()
+    if len(l) > 5:
+        print("Toooooo biiiiiig")
+        return 'nope'
+    else:
+        instr = instr + '0'*(2-len(l[2:])) + l[2:]
+
+    instr = instr + '001'
+    return instr
+
+def build_dat_file(note_list, note_dict):
+    for note in note_list:
+        
+
 notes = create_dict()
 print(notes['A#1'])
-instr = build_note_string(notes, 1, 'wt', 'C1')
+instr = build_note_string(notes, 1, 'w', 'C1')
 print(instr)
-instr = build_note_string(notes, 4, 'hd', 'B6')
+instr = build_note_string(notes, 4, 'h', 'B6')
+print(instr)
+instr = rest(4, 'q')
+print(instr)
+instr = wait(3)
 print(instr)
