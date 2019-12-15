a812fba11d09612e1994fbf35d6f870f7935c8ba
JonahSpicher <jonah.spicher@gmail.com>
2019-12-15 13:47:04 -0500

    Made zelda melody

--- Music/notes_to_hex.py
+++ Music/notes_to_hex.py
@@ -32,6 +32,8 @@ def build_note_string(freqs, channel, length, note, bpm=120):
         l = (120/bpm) * 8
     elif length[0] == 's':
         l = (120/bpm) * 4
+    elif length[0] == 't':
+        l = (120/bpm) * 2
     if len(length) == 2: #Just never make it three please
         if length[1] == 't':
             l *= float(2)/3
@@ -69,6 +71,8 @@ def rest(channel, length, bpm=120):
         l = (120/bpm) * 8
     elif length[0] == 's':
         l = (120/bpm) * 4
+    elif length[0] == 't':
+        l = (120/bpm) * 2
     if len(length) == 2: #Just never make it three please
         if length[1] == 't':
             l *= float(2)/3
@@ -84,9 +88,32 @@ def rest(channel, length, bpm=120):
     instr = instr + '001'
     return instr
 
+def start():
+    return '00000000'
+
+def end():
+    return '11000000'
+
 def build_dat_file(note_list, note_dict):
+    f = open('notes.dat', mode='w')
+    f.write(start() + '\n')
     for note in note_list:
-        
+        if len(note) == 3:
+            #regular note
+            #print(note)
+            instr = build_note_string(note_dict, note[0], note[1], note[2])
+            f.write(instr + '\n')
+        elif len(note) == 2:
+            #rest
+            instr = rest(note[0], note[1])
+            f.write(instr + '\n')
+        elif len(note) == 1:
+            instr = wait(note[0])
+            f.write(instr + '\n')
+    f.write(end())
+    f.close()
+
+
 
 notes = create_dict()
 print(notes['A#1'])
@@ -98,3 +125,19 @@ instr = rest(4, 'q')
 print(instr)
 instr = wait(3)
 print(instr)
+zelda_mel = [[1, 'q', 'A#4'], [1], [1, 'q'], [1], [1, 'qt'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1],
+      [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'et', 'A#4'], [1], [1, 'et'], [1], [1, 'st', 'G#4'], [1], [1, 'st'], [1],
+      [1, 'q', 'A#4'], [1], [1, 'qt'], [1],
+      [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1],
+      [1, 'et', 'A#4'], [1], [1, 'et'], [1], [1, 'st', 'G#4'], [1], [1, 'st'], [1], [1, 'q', 'A#4'], [1], [1, 'qt'], [1],
+      [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'st', 'A#4'], [1], [1, 'st'], [1],
+      [1, 'st', 'A#4'], [1], [1, 'st'], [1], [1, 'e', 'A#4'], [1],
+      [1, 't', 'F4'], [1], [1, 't'], [1], [1, 't', 'F4'], [1], [1, 't'], [1], [1, 'sd', 'F4'], [1], [1, 't'], [1],
+      [1, 't', 'F4'], [1], [1, 't'], [1], [1, 't', 'F4'], [1], [1, 't'], [1], [1, 'sd', 'F4'], [1], [1, 't'], [1],
+      [1, 't', 'F4'], [1], [1, 't'], [1], [1, 't', 'F4'], [1], [1, 't'], [1], [1, 'sd', 'F4'], [1, 't'], [1], [1, 'e', 'F4'], [1],
+      [1, 'q', 'A#4'], [1], [1, 'q', 'F4'], [1], [1, 'ed', 'F4'], [1], [1, 't', 'A#4'], [1], [1, 't'], [1],
+      [1, 's', 'A#4'], [1], [1, 's', 'C5'], [1], [1, 's', 'D5'], [1], [1, 's', 'D#5'], [1], [1, 'h', 'F5'], [1],
+      [1, 'e'], [1], [1, 'sd', 'F5'], [1], [1, 't'], [1], [1, 'et', 'F5'], [1], [1, 'et', 'F#5'], [1], [1, 'et', 'G#5'], [1],
+      [1, 'h', 'A#5'], [1], [1, 'et'], [1], [1, 'st', 'A#5'], [1], [1, 'st'], [1], [1, 'st', 'A#5'], [1], [1, 'st'], [1], [1, 'et', 'A#5'], [1],
+      [1, 'et', 'G#5'], [1], [1, 'et', 'F#5'], [1], [1, 'et', 'G#5'], [1], [1, 'et'], [1], [1, 'et', 'F#5'], [1], [1, 'h', 'F5'], [1]]
+build_dat_file(zelda_mel, notes)
